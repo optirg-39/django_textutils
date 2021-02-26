62d42bfe5855420f1bf722277185e12b27514bb0
rishabh gupta <opti39rg@gmail.com>
2021-02-26 15:51:50 +0530

    first commit

--- textutils/views.py
+++ textutils/views.py
@@ -4,46 +4,47 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,"index.html")
-    # return HttpResponse('<h1>hello world Rishabh</h1>')
 
 def analyzer(request):
-    djtext = request.GET.get('text','default')
-    print(djtext)
-    removepunc = request.GET.get('yesorno', 'yes')
-    capitalize_first = request.GET.get('capitalize', 'yes')
-    newlineremover = request.GET.get('newlineremover', 'yes')
-    extraspaceremover = request.GET.get('extraspaceremover', 'yes')
-    charcount = request.GET.get('charcount', 'yes')
-
-    answer_text = ''
-    print(djtext)
+    djtext = request.POST.get('text','default')
+    removepunc = request.POST.get('yesorno', 'yes')
+    capitalize_first = request.POST.get('capitalize', 'yes')
+    newlineremover = request.POST.get('newlineremover', 'yes')
+    extraspaceremover = request.POST.get('extraspaceremover', 'yes')
+    charcount = request.POST.get('charcount', 'yes')
 
     punctuations='''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     if removepunc=="on":
+        answer_text = ''
         for tx in djtext:
             if tx not in punctuations:
                 answer_text+=tx
-        print(answer_text)
-        print(djtext)
+        djtext=answer_text
         param = {'Purpose': 'Remove Punctuations', "analyzed_text": answer_text}
-    elif capitalize_first =="on":
+    if capitalize_first =="on":
+        answer_text = ''
         for char in djtext:
             answer_text+=char.upper()
-        print(answer_text)
+        djtext = answer_text
         param = {'Purpose': 'CAPITALIZE FIRST', "analyzed_text": answer_text}
-    elif newlineremover =="on":
+    if newlineremover =="on":
+        answer_text = ''
         for char in djtext:
-            if char !="\n":
+            if char !="\n" and char !="\r":
                 answer_text+=char
+        djtext = answer_text
         param = {'Purpose': 'newlineremover', "analyzed_text": answer_text}
-    elif extraspaceremover =="on":
+    if extraspaceremover =="on":
+        answer_text = ''
         for index,char in enumerate(djtext,0):
             if index==(len(djtext)-1):
                 pass
             elif not (djtext[index]==" " and djtext[index+1]==" "):
                 answer_text+=char
+        djtext = answer_text
         param = {'Purpose': 'extraspaceremover', "analyzed_text": answer_text}
-    elif charcount == "on":
+    if charcount == "on":
+        answer_text = ''
         d={}
         for char in djtext:
             if char in d.keys():
@@ -52,8 +53,8 @@ def analyzer(request):
                 d[char]=djtext.count(char)
         answer_text = d
         param = {'Purpose': 'charcount', "analyzed_text": answer_text}
-    else:
-        return HttpResponse('Error occur')
+    if charcount != "on" and extraspaceremover !="on" and newlineremover !="on" and capitalize_first !="on" and removepunc !="on":
+        return render(request, 'valid.html')
 
     return render(request,'analyzer.html', param)
 
